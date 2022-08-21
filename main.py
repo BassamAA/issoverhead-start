1c69c2a3e5e0e90440ae84d398c1faf58a6fbd5e
Bassam Alassaad <alassaadbassam@gmail.com>
2022-08-21 10:59:11 +0300

    enter your own email and password in EMAIL and PASSWORD for program to run

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
+EMAIL = "bassamstesting22@gmail.com"
+PASSWORD = ""
+receiver = "alassaadbassam@gmail.com"
+
+def is_iss_overhead():
+
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG - 5 <= iss_longitude <= MY_LONG:
+        return True
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+def is_night_time():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters,verify=False)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    if sunset["hour"] < time_now["hour"] < sunrise["hour"]:
+        return True
+
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+
+while True:
+    time.sleep(600)
+    if is_iss_overhead() and is_night_time():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=EMAIL,
+                                to_addrs=f"{receiver}",
+                                msg="Look up in the sky to see the ISS !")
+
+
+
+
